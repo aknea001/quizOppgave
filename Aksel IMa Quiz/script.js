5fc6f098a7ca444b963eb5734036cae37fc9c66b
keali0 <aksel.nergaard@outlook.com>
2023-11-05 22:41:41 +0100

    Update script.js

--- Aksel IMa Quiz/script.js
+++ Aksel IMa Quiz/script.js
@@ -1,8 +1,5 @@
 const knapp = document.getElementById("startKnapp")
-let counter = 0
 
 knapp.addEventListener("click", function () {
-    console.log("hello")
-    counter += 1
-    window.location="oppgave" + counter + ".html"
+    window.location="oppgave1.html"
 })
