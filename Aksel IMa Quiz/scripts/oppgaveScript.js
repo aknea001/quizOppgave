fa06942f6848877a45cf2ce8ba85c2e579fcd3e7
aknea001 <aknea001@osloskolen.no>
2023-11-07 13:36:48 +0100

    Update oppgaveScript.js

--- Aksel IMa Quiz/scripts/oppgaveScript.js
+++ Aksel IMa Quiz/scripts/oppgaveScript.js
@@ -5,8 +5,7 @@ let hearts = 5
 feil.forEach(function (feilButton) {
     feilButton.addEventListener("click", function () {
         hearts -= 1
-        console.log(hearts)
-        heartDisplay.src = "4hearts.png"
+        heartDisplay.src = "../bilder/hearts/" + hearts + "hearts.png"
         if (hearts <= 0) {
             window.location = "feil.html"
         }
