f3172690a9c48adbf0a186c7dcd1268fd685261f
aknea001 <aknea001@osloskolen.no>
2023-11-09 18:43:11 +0100

    dad fixed

--- Aksel IMa Quiz/scripts/oppgaveScript.js
+++ Aksel IMa Quiz/scripts/oppgaveScript.js
@@ -5,7 +5,7 @@ let hearts = 5
 feil.forEach(function (feilButton) {
     feilButton.addEventListener("click", function () {
         hearts -= 1
-        heartDisplay.src = "../bilder/hearts/" + hearts + "hearts.png"
+        heartDisplay.src = "bilder/hearts/" + hearts + "hearts.png"
         if (hearts <= 0) {
             window.location = "feil.html"
         }
